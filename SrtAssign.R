eaa6915dd891be731af297d08c53a17c50055410
Ajay Jiwanand <ajay250496@live.com>
2015-03-20 10:28:43 -0400

    srt

--- SrtAssign.R
+++ SrtAssign.R
@@ -1,3 +1,5 @@
+library(dplyr)
+library(igraph)
 phase1 <- read.csv("Phase1.csv")
 phase1$No. <- NULL
 phase1$Time <- NULL
@@ -5,6 +7,25 @@ phase1$Protocol <- NULL
 phase1$Length <- NULL
 phase1 <- filter(phase1, grepl("reply*", Info))
 phase1 <- filter(phase1, grepl("202.77.162.213", Destination))
-phase1$weight <- (c(1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4))
-plot(g,layout=layout.fruchterman.reingold(g,weights=E(g)$weight^3))
+phase1$weight <- (c(4,4,4,3,3,3,3,3,2,2,2,2,1,1,1,1,1,1))
 g <- graph.data.frame(phase1, directed=TRUE)
+plot(g, edge.width=E(g)$weight)
+
+
+
+Phase2 <- read.csv("Phase2.csv")
+Phase1$No. <- NULL
+Phase2$No. <- NULL
+Phase2$Protocol <- NULL
+Phase2$Length\ <- NULL
+Phase2$Length <- NULL
+sadmind <- filter(Phase2, grepl("Port:3*", Info))
+failed <- filter(Phase2, grepl("GETPORT Call *", Info))
+failed <- unique(Phase2$Destination)
+
+if (grepl("Port:3*", Phase2$Info) == TRUE) {
+  Phase2$success <-'yes' } 
+else {
+  Phase2$success <- 'no' }
+
+
